524fa92ec221a5c02b61380634914d18da162dcf
sroesch <sroesch@amadeus.com>
2017-09-21 11:02:00 -0400

    initial test

--- truc.java
+++ truc.java
@@ -15,6 +15,7 @@ class Planet{
     int _canAssign;
     int _owner;
     List<Planet> _neighbors;
+    int _distanceFromClosestEnemy;
 }
 
 class Player {
@@ -62,6 +63,46 @@ class Player {
                 if (theBoard._planets.get(i)._myUnits < theBoard._planets.get(i)._otherUnits){
                     theBoard._planets.get(i)._owner = -1;
                 }
+            }
+
+            //calculate distances from enemy
+            for (int i = 0; i < planetCount; i++) {
+                //reinit
+                if (theBoard._planets.get(i)._owner == -1) {
+                    theBoard._planets.get(i)._distanceFromClosestEnemy = 0;
+                }
+                else {
+                    //-1 here means "not yet calculated"
+                    theBoard._planets.get(i)._distanceFromClosestEnemy = -1;
+                }
+            }
+            //now, increment by increment, let's populate the neighbours
+            boolean stillSomePlanetToCheck=true;
+            //each loop we will only allocate this distance
+            int currentDistance=1;
+            while (stillSomePlanetToCheck) {
+                stillSomePlanetToCheck=false;
+                System.err.println("checking round");
+                //those not yet calculated that are neighbour of a calculated get's its distance+1
+                for (int i = 0; i < planetCount; i++) {
+                    Planet planetToCheck=theBoard._planets.get(i);
+                    if (planetToCheck._distanceFromClosestEnemy == -1) {
+                        stillSomePlanetToCheck=true;
+                        int minNeighborsDist=10000;
+                        for (int j = 0; j < planetToCheck._neighbors.size(); j++) {
+                            if (planetToCheck._neighbors.get(j)._distanceFromClosestEnemy!=-1
+                                        && planetToCheck._neighbors.get(j)._distanceFromClosestEnemy<minNeighborsDist) {
+                                minNeighborsDist=planetToCheck._neighbors.get(j)._distanceFromClosestEnemy;
+                            }
+                        }
+                        if (minNeighborsDist!=10000
+                                && currentDistance==minNeighborsDist+1) {
+                            planetToCheck._distanceFromClosestEnemy = currentDistance;
+                            System.err.println(i + " is at distance " + theBoard._planets.get(i)._distanceFromClosestEnemy + " from ennemy");
+                        }
+                    }
+                }
+                currentDistance++;
             }
 
             // Write an action using System.out.println()
