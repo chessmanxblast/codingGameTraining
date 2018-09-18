34486207f58fbbb644001f9b4e2076e4b353447e
sroesch <sroesch@amadeus.com>
2018-09-18 18:20:28 -0400

    2018 session

---
+++ UTG_2018.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+//constants
+
+//structures
+class Board{
+}
+
+//utils
+
+//if we do alpha-beta
+
+// if we do genetic algo
+//inspired from https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3
