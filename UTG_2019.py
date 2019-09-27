fae66728de16162ef668a77de95b47759c152ea7
Hal PELTONEN <hal.peltonen@amadeus.com>
2019-09-27 09:08:23 -0400

    added radar logic

--- UTG_2019.py
+++ UTG_2019.py
@@ -15,7 +15,7 @@ HOLE = 1
 RADAR = 2
 TRAP = 3
 AMADEUSIUM = 4
-IDEALRADARLOCATIONS = [[11,4],[12,11],[17,7],[23,4],[24,11],[29,7]]
+
 
 
 
@@ -26,7 +26,33 @@ class Pos:
 
     def distance(self, pos):
         return abs(self.x - pos.x) + abs(self.y - pos.y)
+        
+    def changeXYtoAdjacentNonDanger(self,dGrid):
+        if dGrid.get_cell(self.x,self.y).danger == True:
+            if dGrid.get_cell(self.x,self.y-1).danger == False and y > 0:
+                self.y =  self.y - 1
+            elif dGrid.get_cell(self.x-1,self.y).danger == False and x > 1:
+                self.x =  self.x - 1
+            elif dGrid.get_cell(self.x-1,self.y-1).danger == False and x > 1 and y > 0 :
+                self.x =  self.x - 1
+                self.y =  self.y - 1
+            elif dGrid.get_cell(self.x-1,self.y+1).danger == False and x > 1 and y < 14:
+                self.x =  self.x - 1
+                self.y =  self.y + 1
+            elif dGrid.get_cell(self.x+1,self.y-1).danger == False and x < 29 and y > 0 :
+                self.x =  self.x + 1
+                self.y =  self.y - 1
+            elif dGrid.get_cell(self.x+1,self.y).danger == False and x < 29 :
+                self.x =  self.x + 1
+            elif dGrid.get_cell(self.x,self.y+1).danger == False and y < 14 :
+                self.y =  self.y + 1
+            elif dGrid.get_cell(self.x+1,self.y+1).danger == False and x < 29 and y < 14 :
+                self.x =  self.x + 1
+                self.y =  self.y + 1
+            
+        
 
+IDEALRADARLOCATIONS = [Pos(11,4),Pos(12,11),Pos(17,7),Pos(23,4),Pos(24,11),Pos(29,7),Pos(18,14),Pos(5,11)]
 
 class Entity(Pos):
     def __init__(self, x, y, type, id):
@@ -165,7 +191,7 @@ while True:
 
     #game.grid.print()
 
-    # mark the cells with "-111" so that we don't blow up our own robots
+    # mark the cells we put traps in as danger cells so that we don't blow up our own robots
     for trap in game.traps:
         for cell in game.grid.cells:
             if cell.x == trap.x and cell.y == trap.y:
@@ -175,6 +201,14 @@ while True:
     
     currentGame=copy.deepcopy(game)
     
+    #looks at how many radars have been placed, uses that to pull the location of the next radar from the list IDEALRADARLOCATIONS and create a position object NEXTRADARPOS
+    NUMBEROFRADARS = len(game.radars)
+    if NUMBEROFRADARS < len(IDEALRADARLOCATIONS):
+        NEXTRADARPOS = Pos(IDEALRADARLOCATIONS[NUMBEROFRADARS].x,IDEALRADARLOCATIONS[NUMBEROFRADARS].y)
+        if dangerGrid.get_cell(NEXTRADARPOS.x,NEXTRADARPOS.y).danger == True:
+            NEXTRADARPOS.changeXYtoAdjacentNonDanger(dangerGrid)
+            
+    #calculate closest ore to mine for each robot, if there is a multi-ore and a trap is ready, grab it to place on the multiore
     for robot in range(5):
         g_shortest_dist = 999999
         g_curr_robot = game.my_robots[0]
@@ -200,57 +234,55 @@ while True:
         print(g_curr_robot.action, file=sys.stderr)
         print("shortest distance", g_shortest_dist, "robot #:", g_curr_robot.id, "robot_x", g_curr_robot.x, "robot_y", g_curr_robot.y, "cur_cell_x", g_curr_cell.x, "cur_cell_y", g_curr_cell.y, file=sys.stderr)
 
-    
-    #looks at how many radars have been placed, uses that to pull the location of the next radar from the list IDEALRADARLOCATIONS and create a position object NEXTRADARPOS
-    NUMBEROFRADARS = len(game.radars)
-    if NUMBEROFRADARS < 6:
-        NEXTRADARXY = IDEALRADARLOCATIONS[NUMBEROFRADARS]
-        NEXTRADARPOS = Pos(NEXTRADARXY[0],NEXTRADARXY[1])
-    #print(NUMBEROFRADARS, file=sys.stderr)
-    #print(NEXTRADARPOS.x, file=sys.stderr)
-    #print(NEXTRADARPOS.y, file=sys.stderr)
+    if nbTurn > 1:
+        for i in range(len(game.my_robots)):
+            # Write an action using print
+            # To debug: print("Debug messages...", file=sys.stderr)
 
-    #game.my_robots[2].item=AMADEUSIUM
-    
-    for i in range(len(game.my_robots)):
-        # Write an action using print
-        # To debug: print("Debug messages...", file=sys.stderr)
+            # WAIT|
+            # MOVE x y|REQUEST item
 
-        # WAIT|
-        # MOVE x y|REQUEST item
+           
+            #game.my_robots[i].wait(f"Starter AI {i}")
 
-       
-        #game.my_robots[i].wait(f"Starter AI {i}")
-
-        #game.my_robots[i].action="MOVE 3 7"
-        
-        
-        #bring back ore if it has some
-        if game.my_robots[i].item==AMADEUSIUM:
-            game.my_robots[i].action= "MOVE 0 "+ str(game.my_robots[i].y)
-       
-        # if radar will spawn within 2 turns, get guy
-        elif game.radar_cooldown < 2 and game.my_robots[i].item != RADAR and game.my_robots[i].item != TRAP and (game.my_robots[i].action  == 'WAIT'): # or game.my_robots[i].action == 'REQUEST TRAP'):
-            game.my_robots[i].action = f"REQUEST RADAR {i}"
-        elif game.my_robots[i].item == RADAR:
-            game.my_robots[i].action = f"DIG {NEXTRADARPOS.x} {NEXTRADARPOS.y} dropping radar {i}"
-        # else:
-        #     game.my_robots[i].action = f"WAIT waiting for radar to refresh {i}"
-        
-    if nbTurn>1:
-        for i in range(len(game.grid.cells)):
-            currCell=currentGame.grid.cells[i]
-            prevCell=previousGame.grid.cells[i]
+            #game.my_robots[i].action="MOVE 3 7"
             
-            if (currCell.amadeusium!="?" and prevCell.amadeusium!="?" and currCell.amadeusium!=prevCell.amadeusium) or (currCell.hole!=prevCell.hole):
-                #was there an enemy robot near it?
-                for enemyRobot in previousGame.enemy_robots:
-                    if enemyRobot.distance(prevCell)<=1:
-                        print(f"cell {currCell.x} {currCell.y} got possibly dug by an enemy", file=sys.stderr)
-                        dangerGrid.cells[i].danger=True;
-    previousGame=copy.deepcopy(currentGame)
+            
+            #bring back ore if it has some
+            if game.my_robots[i].item==AMADEUSIUM:
+                game.my_robots[i].action= "MOVE 0 "+ str(game.my_robots[i].y)
+           
+
+            #if a robot has radar, drop it at next pos
+            elif game.my_robots[i].item == RADAR:
+                game.my_robots[i].action = f"DIG {NEXTRADARPOS.x} {NEXTRADARPOS.y} dropping radar {i}"
+            # else:
+            #     game.my_robots[i].action = f"WAIT waiting for radar to refresh {i}"
+            
+            for i in range(len(game.grid.cells)):
+                currCell=currentGame.grid.cells[i]
+                prevCell=previousGame.grid.cells[i]
+                
+                if (currCell.amadeusium!="?" and prevCell.amadeusium!="?" and currCell.amadeusium!=prevCell.amadeusium) or (currCell.hole!=prevCell.hole):
+                    #was there an enemy robot near it?
+                    for enemyRobot in previousGame.enemy_robots:
+                        if enemyRobot.distance(prevCell)<=1:
+                            print(f"cell {currCell.x} {currCell.y} got possibly dug by an enemy", file=sys.stderr)
+                            dangerGrid.cells[i].danger=True;
     
+    #if a radar is almost ready, the robot closest to the efficient point at the base should grab it
+    
+    if game.radar_cooldown < 2:
+        shortestDistanceToRadarHQPoint = 999
+        for j in range(5):
+            robotdistanceToRadarHQPoint = game.my_robots[j].distance(Pos(0,NEXTRADARPOS.y))
+            if robotdistanceToRadarHQPoint < shortestDistanceToRadarHQPoint and game.my_robots[j].item != RADAR and game.my_robots[j].item != TRAP:
+                shortestDistanceToRadarHQPoint = robotdistanceToRadarHQPoint 
+                closestRobotToRadarHQPoint = j
+        game.my_robots[closestRobotToRadarHQPoint].action = f"REQUEST RADAR {j}"
+    previousGame=copy.deepcopy(currentGame)
         
+            
     game.my_robots[0].doAction()
     game.my_robots[1].doAction()
     game.my_robots[2].doAction()
